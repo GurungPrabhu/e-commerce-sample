0a57014daced5c44547b9851f7c5f689156bcfe1
unknown <prabhu.eversoft@gmail.com>
2021-06-02 18:42:58 +0545

    [FEATURE]: Form validation

--- src/components/modules/checkout-form/index.jsx
+++ src/components/modules/checkout-form/index.jsx
@@ -1,17 +1,40 @@
-import React from "react"
+import React, { useState } from "react"
 import Button from "../../elements/button"
+import { useNotification } from "../../context/notification.context"
 
 const CheckoutForm = () => {
+  const [formData, setFormData] = useState()
+  const [makeNotification] = useNotification()
+
+  const onChange = (e) => {
+    setFormData({
+      ...formData,
+      [e.target.name]: e.target.value
+    })
+    console.log(formData)
+  }
+
+  const onSubmit = (e) => {
+    e.preventDefault()
+    console.log("data", formData)
+    makeNotification("Order Successfull", "success")
+  }
+
   return (
     <div className="row">
       <h1>Purchase form</h1>
-      <form>
+      <form onSubmit={onSubmit}>
         <div className="form-group my-2">
           <label htmlFor="formGroupExampleInput">Name</label>
           <input
             type="text"
+            name="name"
             className="form-control"
-            id="formGroupExampleInput"
+            id="name"
+            required
+            min="3"
+            value={formData?.name || ""}
+            onChange={onChange}
             placeholder="Prabhu Gurung"
           />
         </div>
@@ -20,7 +43,12 @@ const CheckoutForm = () => {
           <input
             type="text"
             className="form-control"
-            id="formGroupExampleInput2"
+            name="address"
+            value={formData?.address || ""}
+            id="address"
+            required
+            min="3"
+            onChange={onChange}
             placeholder="Pokhara"
           />
         </div>
@@ -29,7 +57,11 @@ const CheckoutForm = () => {
           <input
             type="text"
             className="form-control"
-            id="formGroupExampleInput2"
+            name="email"
+            required
+            id="email"
+            onChange={onChange}
+            value={formData?.email || ""}
             placeholder="something@gmail.com"
           />
         </div>
@@ -45,7 +77,9 @@ const CheckoutForm = () => {
           />
         </div>
         <div className="form-group my-2 mt-4">
-          <Button type="primary">Submit</Button>
+          <Button type="primary" htmlType="submit">
+            Submit
+          </Button>
         </div>
       </form>
     </div>
